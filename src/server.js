0a863221a10476f9b9de330bb2dcd985bd50a3c1
Etiqueta Certa TI Etiqueta Certa TI <gabrielramador2014@gmail.com>
2023-06-01 18:26:08 -0300

    feat: add database and routes

--- src/server.js
+++ src/server.js
@@ -1,37 +1,26 @@
 import http from "node:http";
 
-import { randomUUID } from "node:crypto";
 import { json } from "./middlewares/json.js";
-import { Database } from "./database.js";
-
-const database = new Database()
+import { routes } from "./routes.js";
 
 const server = http.createServer(async (req, res) => {
-    const { method, url } = req;
+  const { method, url } = req;
 
-    await json(req, res)
+  await json(req, res);
 
-    if (method === "GET" && url === "/users") {
-        const users = database.select('users')
-    
-        return res.end(JSON.stringify(users));
-    }
+  const route = routes.find((route) => {
+    return route.method === method && route.path.test(url);
+  });
 
-    if (method === "POST" && url === "/users") {
-        const { name, email } = req.body;
+  if (route) {
+    const routeParams = url.match(route.path);
 
-        const user = {
-            id: randomUUID(),
-            name: name,
-            email: email
-        }
+    req.params = { ...routeParams.groups };
 
-        database.insert('users', user)
+    return route.handler(req, res);
+  }
 
-        return res.writeHead(201).end();
-    }
-
-    return res.writeHead(404).end();
+  return res.writeHead(404).end();
 });
 
 server.listen(3333);
